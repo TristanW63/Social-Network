4e205e62d53078e38397e61dcbb7a772cedd5027
Tristan Barron <tristan_wilson_63@yahoo.com>
2022-12-05 16:39:13 -0600

    thoughts model

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { ObjectId } = require('bson');
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: { type: ObjectId, default: new ObjectId},
+    reactionBody: { type: String, required: true, maxLength: 280},
+    username: { type: String, required: true},
+    createdAt: { type: Date, default: Date.now, formatdate}
+})
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            //format date somehow var date = new Date();
+            //date.toDateString();
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema
+.virtual('reactionCount')
+.get(function () {
+    return this.reactions.length;
+});
+
