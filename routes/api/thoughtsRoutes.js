8ba45e3066d68b5c4600a967661b940c936e997e
Tristan Barron <tristan_wilson_63@yahoo.com>
2022-12-06 15:08:12 -0600

    finishing thoughts and user routes

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -6,14 +6,20 @@ const {
     createThought,
     updateThought,
     deleteThought,
+    addReaction,
+    removeReaction,
 } = require('../../controllers/thoughtsController');
 
+// /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-router
-    .route('/:thoughtId')
-    .get(getSingleUser)
-    .put(updateUser)
-    .delete(deleteUSer);
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).put(updateThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/thoughts/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
