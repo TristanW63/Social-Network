3df1924d3c1b48d23cd264a5a3c825a4946debde
Tristan Barron <tristan_wilson_63@yahoo.com>
2022-12-07 15:55:34 -0600

    thoughtsController

---
+++ controllers/usersController.js
@@ -0,0 +1,16 @@
+const { ObjectId } = require('mongoose').Types;
+const { Users, Thought } = require('../models');
+
+module.exports = {
+    //Get all Users
+    getUsers(req,res) {
+        Users.find()
+         .then((users) => res.json(users))
+         .catch((err) => res.status(500).json(err));
+    },
+    // Get single user 
+    getSingleUser(req,res) {
+        Users.findOne({_id: req.params.userId })
+
+    }
+}
