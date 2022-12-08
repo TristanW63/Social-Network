ccaabbcfb96c630c710b745daf683b67f15ac0c9
Tristan Barron <tristan_wilson_63@yahoo.com>
2022-12-08 13:30:13 -0600

    userscontroller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,16 +1,41 @@
-const { ObjectId } = require('mongoose').Types;
-const { Users, Thought } = require('../models');
+const { ObjectId } = require("mongoose").Types;
+const { Users, Thought } = require("../models");
 
 module.exports = {
-    //Get all Users
-    getUsers(req,res) {
-        Users.find()
-         .then((users) => res.json(users))
-         .catch((err) => res.status(500).json(err));
-    },
-    // Get single user 
-    getSingleUser(req,res) {
-        Users.findOne({ _id: req.params.userId })
+  //Get all Users
+  getUsers(req, res) {
+    Users.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get single user
+  getSingleUser(req, res) {
+    Users.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 
-    }
-}
+  createUser(req, res) {
+    Users.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteUser(req, res) {
+    Users.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : Thought.deleteMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() =>
+        res.json({ message: "User and associated thoughts deleted!" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
